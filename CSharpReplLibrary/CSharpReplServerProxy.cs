2635beecb80b7371083baaff235f3738bffb636a
Dustin Mays <dustin.nathaniel.mays@gmail.com>
2014-03-26 16:36:32 -0400

    Lots of refactoring, added convenience constructors, and error handling for invalid connection information. The private Send() and Receive() methods are also now properly async

--- CSharpReplLibrary/CSharpReplServerProxy.cs
+++ CSharpReplLibrary/CSharpReplServerProxy.cs
@@ -1,135 +1,128 @@
 using System;
-using System.Net;
-using NetMQ;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Mono.CSharp;
-using System.Runtime.Remoting.Lifetime;
-using System.Net.Configuration;
+using NetMQ;
 
 namespace MonoDevelop.CSharpRepl
 {
 	//	public class CSharpReplServerProxy : ICSharpRepl, IDisposable
 	public class CSharpReplServerProxy : IAsyncCSharpRepl, IDisposable
 	{
-		private  NetMQContext nmqContext;
-		private  NetMQSocket nmqClient;
-		private  Poller nmqPoller;
-		private  NetMQScheduler nmqScheduler;
-		private  string address;
+		private NetMQContext nmqContext;
+		private NetMQSocket nmqClient;
+		private Poller nmqPoller;
+		private NetMQScheduler nmqScheduler;
 
-		public CSharpReplServerProxy (string address)
+		public Uri Address { get; private set; }
+
+		protected CSharpReplServerProxy ()
 		{
 			nmqContext = NetMQContext.Create ();
+		}
 
+		public CSharpReplServerProxy (string protocol, string host, int port) : this (String.Format ("{0}://{0}:{1}", protocol, host, port))
+		{
+		}
 
-			this.address = address;
+		public CSharpReplServerProxy (string host, int port) : this (String.Format ("tcp://{0}:{1}", host, port))
+		{
+		}
+
+		public CSharpReplServerProxy (string uri) : this (new Uri (uri))
+		{
+		}
+
+		public CSharpReplServerProxy (Uri uri) : this ()
+		{
+			if (uri == null)
+				throw new ArgumentNullException ("uri");
+
+			var builder = new UriBuilder (uri);
+
+			if (string.IsNullOrWhiteSpace (builder.Host))
+				throw new ArgumentException ("Must supply a valid hostname or IP", "uri");
+
+			if (string.IsNullOrWhiteSpace (builder.Scheme)) {
+				builder.Scheme = "tcp";
+				// debug message here
+			}
+
+			if (builder.Port == 0) {
+				builder.Port = NetworkUtilities.GetOpenPort ();
+				// debug message here
+			}
+
+			Address = builder.Uri;
 		}
 
 		public void Start ()
 		{
 			ThrowIfDisposed ();
-
 			nmqPoller = new Poller ();
 			nmqClient = nmqContext.CreateRequestSocket ();
-
-			nmqClient.Connect (address);
+			nmqClient.Connect (Address.AbsoluteUri.TrimEnd ('/'));
 			nmqScheduler = new NetMQScheduler (nmqContext, nmqPoller);
 			Task.Factory.StartNew (() => nmqPoller.Start (), TaskCreationOptions.LongRunning);
-
 		}
 
-		internal async Task Send (Request request)
+		private void Send (Request request)
 		{
-			byte[] bytes = request.Serialize ();
-			Task sendTask = new Task (() => nmqClient.Send (bytes));
+			byte[] data = request.Serialize ();
+			Task sendTask = new Task (() => nmqClient.Send (data));
 			sendTask.Start (nmqScheduler);
-			await sendTask;
 		}
 
-		internal async Task<Result> Receive ()
+		private async Task<Result> Receive ()
 		{
-
-			//var resultTask = new Task<byte[]> (() => {
-
-			//});
-			//resultTask.Start (nmqScheduler);
-			//var result = await resultTask;
-			//			return nmqClient.Receive ();
-			return Result.Deserialize (nmqClient.Receive ());
-		}
-		/*
-		internal async Task<Result> ReceiveMessage ()
-		{
-			ThrowIfDisposed ();
-			return await Task.Factory.StartNew<Result> (() => {
-				bool has_more;
-				byte[] incoming_buffer = nmqClient.Receive (out has_more);
-				while (has_more)
-					incoming_buffer.Concat (nmqClient.Receive (out has_more));
-				var result = Result.Deserialize (incoming_buffer);
-				return result;
+			var resultTask = new Task<byte[]> (() => {
+				var data = nmqClient.Receive ();
+				return data;
 			});
+			resultTask.Start (nmqScheduler);
+			await resultTask;
+			var result = Result.Deserialize (resultTask.Result);
+			return result;
 		}
 
-		internal async Task<Result> SendMessage (Request rq)
+		private async Task<Result> sendAndReceive (Request request)
 		{
-			ThrowIfDisposed ();
-			var result = await Task.Factory.StartNew (() => {
-				byte[] outgoing_buffer = rq.Serialize ();
-				nmqClient.Send (outgoing_buffer);
-			}).ContinueWith<Task<Result>> (
-				             task => ReceiveMessage ());
-
-			return await result;
+			Send (request);
+			return await Receive ();
 		}
-		*/
-
-		#region ICSharpShell implementation
 
 		public async Task<Result> evaluate (string input)
 		{
 			ThrowIfDisposed ();
-			var request = Request.CreateEvaluationRequest (input);
-			await Send (request);
-			Console.WriteLine ("getting ready to receive");
-			var result = await Receive ();
-			return result;
+			return await sendAndReceive (Request.CreateEvaluationRequest (input));
 		}
 
 		public async Task<Result> loadAssembly (string file)
 		{
 			ThrowIfDisposed ();
-			var request = Request.CreateLoadAssemblyRequest (file);
-			await Send (request);
-			var result = await Receive ();
-			return result;
+			return await sendAndReceive (Request.CreateLoadAssemblyRequest (file));
 		}
 
 		public async Task<Result> getVariables ()
 		{
 			ThrowIfDisposed ();
-			var request = Request.CreateGetVariablesRequest ();
-			await Send (request);
-			var result = await Receive ();
-			return result;
+			return await sendAndReceive (Request.CreateGetVariablesRequest ());
 		}
 
 		public async Task<Result> getUsings ()
 		{
 			ThrowIfDisposed ();
-			var request = Request.CreateGetUsingsRequest ();
-			await Send (request);
-			var result = await Receive ();
-			return result;
+			return await sendAndReceive (Request.CreateGetUsingsRequest ());
 		}
 
 		private void ThrowIfDisposed ()
 		{
-			if (!disposed)
-				return;
-			throw new ObjectDisposedException (this.GetType ().Name);
+			if (disposed)
+				throw new ObjectDisposedException (this.GetType ().Name);
 		}
 
 		bool disposed;
@@ -137,29 +130,28 @@ namespace MonoDevelop.CSharpRepl
 		public void Dispose ()
 		{
 			ThrowIfDisposed ();
-			if (!disposed)
-			{
-				if (nmqClient != null)
-				{
+			if (!disposed) {
+				if (nmqClient != null) {
+					nmqPoller.RemoveSocket (nmqClient);
 					nmqClient.Close ();
 					nmqClient.Dispose ();
 				}
-				if (nmqScheduler != null)
-					nmqScheduler.Dispose ();
-				if (nmqPoller != null)
-				{
-					nmqPoller.Stop ();
+				if (nmqPoller != null) {
+					nmqPoller.Stop (false);
 				}
-				if (nmqContext != null)
-				{
+				if (nmqScheduler != null) {
+					nmqScheduler.Dispose ();
+				}
+				if (nmqContext != null) {
 					nmqContext.Terminate ();
 					nmqContext.Dispose ();
 				}
+				nmqClient = null;
+				nmqPoller = null;
+				nmqScheduler = null;
+				nmqContext = null;
 				disposed = true;
 			}
 		}
-
-		#endregion
 	}
 }
-
