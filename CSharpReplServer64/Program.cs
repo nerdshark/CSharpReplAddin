c57b43e29f8f5a1dcb2f67f7e4d09add8351b68d
Dustin Mays <dustin.nathaniel.mays@gmail.com>
2014-03-26 18:45:06 -0400

    Removing startup debug message

--- CSharpReplServer64/Program.cs
+++ CSharpReplServer64/Program.cs
@@ -9,7 +9,6 @@ namespace CSharpReplServer64
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Starting C# interactive shell on port {0}", args [0]);
 			MonoDevelop.CSharpRepl.CSharpReplServer.Run ("127.0.0.1", Int32.Parse (args [0])).Wait ();
 		}
 	}
